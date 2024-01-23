73583b6c64e405d9b9f07c686581b9d4476f36aa
swadebsharma6 <swadebriva2012@gmail.com>
2024-01-23 11:12:53 +0600

    initial the project with react and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <>
+      <section className="">
+        <h1 className="text-3xl font-bold my-5  text-blue-700 text-center">Welcome to Movie Retails</h1>
+      </section>
+    </>
+  );
+};
+
+export default App;
